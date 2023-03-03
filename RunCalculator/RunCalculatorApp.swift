e6f33f50e94e0b23182bc9f89d3f39566fc4f22b
Mike Muszynski <mmuszynski@gmail.com>
2023-03-03 11:04:00 -0500

    Initial Commit

---
+++ RunCalculator/RunCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunCalculatorApp.swift
+//  RunCalculator
+//
+//  Created by Mike Muszynski on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct RunCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
